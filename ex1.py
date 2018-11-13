bb6c88c7dd679241780afa785c4995d3755b9b57
lidianxiang <39815521+lidianxiang@users.noreply.github.com>
2018-11-13 22:52:30 +0800

    习题1.第一个程序

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("Hello world!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun")
+print("Yay! Printing.")
+print("I'd much ratther you 'not'.")
+print('I "said" do not touch this.')
