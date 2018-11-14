303da4c9722b0b63713382d1071710c505dd0092
lidianxiang <39815521+lidianxiang@users.noreply.github.com>
2018-11-14 20:09:33 +0800

    习题2.注释与#

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A commet, this is so you can read your program later
+# Anything after the # is ignored by Python
+
+print("I could have code like this.") # and the commet after is ignored
+
+# You can also use a commet to "disable" or commet out code
+#　print("This won't run")
+
+print("This will run")
