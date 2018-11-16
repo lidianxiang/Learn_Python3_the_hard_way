9a7089dc596c06ffbc5a04bc902e8cedbf03f60c
lidianxiang <39815521+lidianxiang@users.noreply.github.com>
2018-11-16 20:38:25 +0800

    习题13. 参数、解包和变量

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS selection for how to run this
+script, first, second, third = argv
+
+print("This script is called:",script)
+print("Your first variable is:",first)
+print("Your second variable is:",second)
+print("Your third variable is:",third)
